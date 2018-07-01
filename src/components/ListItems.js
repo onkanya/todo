d5df50d569a9d91e2bf2d27050d6abc207b6e189
Onkanya Wongrakthai <onkanyalearning@gmail.com>
2018-07-01 10:11:27 +0700

    Add JSON-SERVER

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-class ListItems  extends Component {
-    
-    render() {
-        // let todos = this.todos.map
-        return (
-            <React.Fragment>
-            {this.props.todos.map(item => (
-                <div
-                key = {item.id}
+import React, { Component } from "react";
+class ListItems extends Component {
+  render() {
+    // let todos = this.todos.map
+    return (
+      <React.Fragment>
+        {this.props.todos.map(item => (
+          <div
+            key={item.id}
             style={{
               backgroundColor: "#fefefe",
               borderColor: "#ccc",
@@ -20,32 +19,31 @@ class ListItems  extends Component {
               paddingBottom: 19
             }}
           >
-            <input type="checkbox"/>{item.name}
+            <input type="checkbox" />
+            {item.name}
           </div>
-            ))}
-            </React.Fragment>
-        //     <React.Fragment>
-        // <div
-        //     style={{
-        //       backgroundColor: "#fefefe",
-        //       borderColor: "#ccc",
-        //       borderWidth: 1,
-        //       borderStyle: "solid",
-        //       borderRadius: 5,
-        //       margin: 2,
-        //       padding: 5,
-        //       paddingTop: 10,
-        //       paddingBottom: 19
-        //     }}
-        //   >
-        //     <input type="checkbox" checked /> <s> Shopping</s>
-        //   </div>              
-          
-        //     </React.Fragment>
-                     
-          
-        );
-    }
+        ))}
+      </React.Fragment>
+      //     <React.Fragment>
+      // <div
+      //     style={{
+      //       backgroundColor: "#fefefe",
+      //       borderColor: "#ccc",
+      //       borderWidth: 1,
+      //       borderStyle: "solid",
+      //       borderRadius: 5,
+      //       margin: 2,
+      //       padding: 5,
+      //       paddingTop: 10,
+      //       paddingBottom: 19
+      //     }}
+      //   >
+      //     <input type="checkbox" checked /> <s> Shopping</s>
+      //   </div>
+
+      //     </React.Fragment>
+    );
+  }
 }
 
-export default ListItems ;
+export default ListItems;
