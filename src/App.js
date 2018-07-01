927dfb17ecda2acfe53236231a3937e18460b6ad
Onkanya Wongrakthai <onkanyalearning@gmail.com>
2018-07-01 12:53:30 +0700

    update todos

--- src/App.js
+++ src/App.js
@@ -1,39 +1,60 @@
 import React, { Component } from "react";
-import FormSubmit from './components/FormSubmit';
-import HeaderComponents from './components/HeaderComponents';
+import FormSubmit from "./components/FormSubmit";
+import HeaderComponents from "./components/HeaderComponents";
 import List from "./components/List";
 import ListItems from "./components/ListItems";
+import Axios from "axios";
+//มี . นำหน้า คือค้นหาจากไฟล์ของเรา ถ้าไม่มี . คือหาจาก node_modules
+
+// สร้างตัวแปรเก็บ url เพื่อเรียกใช้ให้ง่ายขึ้น
+const URL ="http://localhost:3001";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        todos: [
-            { id: 1, name: "Shopping", complete: false},
-            { id: 2, name: "Swimming", complete: false},
-            { id: 3, name: "Watch Television", complete: false},
-            { id: 4, name: "Excercise", complete: false}],
-            message: ""
-      };
-      // โคตรงง
-      this.onChangeMessage = this.onChangeMessage.bind(this);
-      this.onSubmitMessage = this.onSubmitMessage.bind(this);
-}
+      todos: [],
+      message: ""
+    };
+    // โคตรงง
+    this.onChangeMessage = this.onChangeMessage.bind(this);
+    this.onSubmitMessage = this.onSubmitMessage.bind(this);
+  }
 
-onChangeMessage(e){
-  this.setState({message: e.target.value});
-}
+  componentDidMount = () => {
+    // Axios.get("http://localhost:3001/todos").then(response => {
+    Axios.get(URL +"/todos").then(response => {
+      // console.log(response);
+      this.setState({todos: response.data});
+    });
+  };
 
-onSubmitMessage(e){
-  // ป้องกันการเปลี่ยนหน้า เช่น submit แล้วไม่ต้อง reload
-  e.preventDefault();
-  let oldTodos = this.state.todos;
-  let todosLength = this.state.todos.length;
-  let lastId = this.state.todos[todosLength - 1].id
-  let newmessage = {id: lastId + 1, name: this.state.message, complete: false};
-  oldTodos.push(newmessage);
-  this.setState({todos: oldTodos});
-}
+  onChangeMessage(e) {
+    this.setState({ message: e.target.value });
+  }
+
+  onSubmitMessage(e) {
+    // ป้องกันการเปลี่ยนหน้า เช่น submit แล้วไม่ต้อง reload
+    // e.preventDefault();
+    // let oldTodos = this.state.todos;
+    // let todosLength = this.state.todos.length;
+    // let lastId = this.state.todos[todosLength - 1].id;
+    // let newmessage = {
+    //   // id: lastId + 1,
+    //   name: this.state.message,
+    //   complete: false
+    // };
+    // oldTodos.push(newmessage);
+    // this.setState({ todos: oldTodos });
+    Axios.post(URL +"/todos",{
+      name: this.state.message,
+      complete: false
+    }).then(response => {
+      let oldState = this.state.todos;
+      oldState.push(response.data);
+      this.setState({todos: oldState});
+    })
+  }
   render() {
     return (
       <div
@@ -47,9 +68,12 @@ onSubmitMessage(e){
           marginTop: 20
         }}
       >
-      <HeaderComponents />        
+        <HeaderComponents />
         <List todos={this.state.todos} />
-        <FormSubmit onChangeMessage={this.onChangeMessage} onSubmitMessage={this.onSubmitMessage} />        
+        <FormSubmit
+          onChangeMessage={this.onChangeMessage}
+          onSubmitMessage={this.onSubmitMessage}
+        />
       </div>
     );
   }
